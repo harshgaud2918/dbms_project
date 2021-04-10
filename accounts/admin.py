c0baa1f2f11d3ffba6e6006c62c2af5eb373ef03
Mercury_80 <harsh.gaud18@gmail.com>
2021-04-10 23:59:50 +0530

    Connected Local Mysql database and created models :)

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import*
 
 # Register your models here.
+admin.site.register(Agent)
+admin.site.register(Property)
+admin.site.register(Buyer)
+admin.site.register(Owner)
+admin.site.register(Tenant)
+admin.site.register(Login)
+admin.site.register(BuySellTransaction)
+admin.site.register(User)
+admin.site.register(RentTransaction)
+
+
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,119 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Agent(models.Model):
+    agent_id = models.DecimalField(primary_key=True, max_digits=16, decimal_places=0)
+    username = models.ForeignKey('Login', models.DO_NOTHING, db_column='username')
+    first_name = models.CharField(db_column='first_Name', max_length=32)  # Field name made lowercase.
+    last_name = models.CharField(db_column='last_Name', max_length=32)  # Field name made lowercase.
+    email = models.CharField(unique=True, max_length=32)
+    contact = models.DecimalField(unique=True, max_digits=10, decimal_places=0)
+
+    class Meta:
+        managed = False
+        db_table = 'agent'
+
+
+
+class BuySellTransaction(models.Model):
+    tsnc_id = models.DecimalField(primary_key=True, max_digits=16, decimal_places=0)
+    date = models.DateField()
+    property = models.ForeignKey('Property', models.DO_NOTHING)
+    buyer = models.ForeignKey('Buyer', models.DO_NOTHING)
+    agent = models.ForeignKey(Agent, models.DO_NOTHING)
+    owner = models.ForeignKey('Owner', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'buy_sell_transaction'
+
+
+class Buyer(models.Model):
+    buyer_id = models.DecimalField(primary_key=True, max_digits=16, decimal_places=0)
+    user = models.ForeignKey('User', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'buyer'
+
+
+
+
+
+class Login(models.Model):
+    username = models.CharField(primary_key=True, max_length=32)
+    password = models.CharField(max_length=32)
+
+    class Meta:
+        managed = False
+        db_table = 'login'
+
+
+class Owner(models.Model):
+    owner_id = models.DecimalField(primary_key=True, max_digits=16, decimal_places=0)
+    user = models.ForeignKey('User', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'owner'
+
+
+class Property(models.Model):
+    property_id = models.DecimalField(primary_key=True, max_digits=16, decimal_places=0)
+    owner = models.ForeignKey(Owner, models.DO_NOTHING)
+    address = models.CharField(max_length=64)
+    city = models.CharField(max_length=32)
+    state = models.CharField(max_length=32)
+    pincode = models.DecimalField(max_digits=6, decimal_places=0)
+    no_of_bedrooms = models.DecimalField(max_digits=2, decimal_places=0)
+    size = models.DecimalField(max_digits=10, decimal_places=0)
+    type = models.CharField(max_length=32)
+    status = models.CharField(max_length=32)
+    amount = models.DecimalField(max_digits=32, decimal_places=0)
+    agent = models.ForeignKey(Agent, models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'property'
+
+
+class RentTransaction(models.Model):
+    tsnc_id = models.DecimalField(primary_key=True, max_digits=16, decimal_places=0)
+    date = models.DateField()
+    property = models.ForeignKey(Property, models.DO_NOTHING)
+    tenant = models.ForeignKey('Tenant', models.DO_NOTHING)
+    agent = models.ForeignKey(Agent, models.DO_NOTHING)
+    owner = models.ForeignKey(Owner, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'rent_transaction'
+
+
+class Tenant(models.Model):
+    tenant_id = models.DecimalField(primary_key=True, max_digits=16, decimal_places=0)
+    user = models.ForeignKey('User', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'tenant'
+
+
+class User(models.Model):
+    user_id = models.DecimalField(primary_key=True, max_digits=16, decimal_places=0)
+    username = models.ForeignKey(Login, models.DO_NOTHING, db_column='username')
+    first_name = models.CharField(db_column='first_Name', max_length=32)  # Field name made lowercase.
+    last_name = models.CharField(db_column='last_Name', max_length=32)  # Field name made lowercase.
+    email = models.CharField(unique=True, max_length=32)
+    contact = models.DecimalField(unique=True, max_digits=10, decimal_places=0)
+
+    class Meta:
+        managed = False
+        db_table = 'user'
