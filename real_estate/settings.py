d4e0e1717de92610fafb70ff760f4b2c9a9e69c7
Mercury_80 <harsh.gaud18@gmail.com>
2021-04-11 22:22:33 +0530

    requirements updated

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +82,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'Aman163Harsh185',
     }
 }
 
