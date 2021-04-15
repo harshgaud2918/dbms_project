3e9ce1f9f0aaa167ce7262e3e093183c53441e8c
Atik <atiknageshwar07@gmail.com>
2021-04-15 22:47:01 +0530

    added login page templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     #path(route='', view=views.index, name="home"),
 	path(route='login', view=views.login, name="login"),
     path(route='register', view=views.register, name="register"),
-	
+	path(route='login_admin', view=views.login_admin, name="login_admin"),
+    path(route='login_agent', view=views.login_agent, name="login_agent")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'login_as.html')
 
 def register(request):
     return render(request, 'register.html')
+
+def login_admin(request):
+    return render(request, 'login_admin.html')
+
+def login_agent(request):
+    return render(request, 'login_agent.html')
