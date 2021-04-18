9c84ebcb2955f2f30fecc553f9660cbbb97d0741
Atik <atiknageshwar07@gmail.com>
2021-04-18 09:20:34 +0530

    some frontend pages

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,15 @@ urlpatterns = [
 	path(route='login', view=views.login, name="login"),
     path(route='register', view=views.register, name="register"),
 	path(route='login_admin', view=views.login_admin, name="login_admin"),
-    path(route='login_agent', view=views.login_agent, name="login_agent")
+    path(route='login_agent', view=views.login_agent, name="login_agent"),
+
+    path(route='agent_properties', view=views.agent_properties, name="agent_properties"),
+
+    path(route='property_agent', view=views.display_property_agent, name="property_agent"),
+    path(route='property_admin', view=views.display_property_admin, name="property_admin"),
+
+    path(route='add_property', view=views.add_property, name="add_property"),
+    path(route='add_user', view=views.add_user, name="add_user"),
+
+    path(route='agent_dash', view=views.agent_dash, name="agent_dash")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -12,3 +12,56 @@ def login_admin(request):
 
 def login_agent(request):
     return render(request, 'login_agent.html')
+
+properties = [
+    {
+        'property_id' : 'abc',
+        'Type' : 'abc',
+        'Size' : 'abc',
+        'no_of_bedrooms' : 'abc',
+        'Address' : 'abc',
+        'Amount' : 'abc',
+        'Status' : 'abc',
+        'agent_id' : 'abc'
+    }
+]
+
+agent_properties = [
+    {
+        'property_id' : 'abc',
+        'Type' : 'abc',
+        'Size' : 'abc',
+        'no_of_bedrooms' : 'abc',
+        'Address' : 'abc',
+        'Amount' : 'abc',
+        'Status' : 'abc'
+    }
+]
+
+def agent_properties(request):
+    context = {
+        'properties' : properties,
+        'agent_properties' : agent_properties
+    }
+    return render(request, 'agent_properties.html', context)
+
+def display_property_agent(request):
+    context = {
+        'property' : 'blah'
+    }
+    return render(request, 'property_agent.html', context)
+
+def display_property_admin(request):
+    context = {
+        'property' : 'blah'
+    }
+    return render(request, 'property_admin.html', context)
+
+def add_property(request):
+    return render(request, 'agent_add_prop.html')
+
+def add_user(request):
+    return render(request, 'agent_add_user.html')
+
+def agent_dash(request):
+    return render(request, 'agent_dash.html')
