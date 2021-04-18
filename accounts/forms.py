6192d97fd13ab8c063e0c6ced7a02f8ce34dded7
Mercury_80 <harsh.gaud18@gmail.com>
2021-04-18 11:06:07 +0530

    Forms created

---
+++ accounts/forms.py
@@ -0,0 +1,47 @@
+from django.forms import ModelForm
+from django import forms
+from .models import *
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class DateTimeInput(forms.DateTimeInput):
+    input_type = 'datetime-local'
+
+class UserRegistrationForm(ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+        
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
+
+
+class AddPropertyForm(ModelForm):
+    class Meta:
+        model = Property
+        fields = '__all__'
+        
+
+    def __init__(self, *args, **kwargs):
+        super(AddPropertyForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
+
+class AddPropertyForm(ModelForm):
+    class Meta:
+        model = Property
+        fields = '__all__'
+        
+
+    def __init__(self, *args, **kwargs):
+        super(AddPropertyForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
